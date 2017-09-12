1761659999f6ff3c023c36d3c122ba6b7fce7b80
Murphy Berzish <murphy.berzish@gmail.com>
2017-09-12 15:15:47 -0400

    add feature to run all benchmarks in run-solver.py

--- run-solver.py
+++ run-solver.py
@@ -24,6 +24,7 @@ import runmanagement
 
 if len(sys.argv) < 4:
     print("Usage: " + sys.argv[0] + " solver-name solver-version benchmark-name [solver-arguments...]")
+    print("If benchmark name is 'all', solver will be run on all registered benchmarks.")
     sys.exit(1)
 
 solverName = sys.argv[1]
@@ -44,6 +45,7 @@ session = Session()
 
 solver = None
 benchmark = None
+runAllBenchmarks = (benchmarkName == 'all')
 
 try:
     solver = session.query(dbobj.Solver, dbobj.SolverVersion).filter(dbobj.Solver.id == dbobj.SolverVersion.solver_id, dbobj.Solver.name == solverName, dbobj.SolverVersion.version == solverVersionName).one_or_none()
@@ -53,28 +55,50 @@ try:
         sys.exit(1)
     # unpack (Solver, SolverVersion)
     solver = solver[1]
-    benchmark = session.query(dbobj.Benchmark).filter(dbobj.Benchmark.name == benchmarkName).one_or_none()
-    if benchmark is None:
-        print("No benchmark %s found." % (benchmarkName,))
-        session.rollback()
-        sys.exit(1)
+    if runAllBenchmarks:
+        
+    else:
+        benchmark = session.query(dbobj.Benchmark).filter(dbobj.Benchmark.name == benchmarkName).one_or_none()
+        if benchmark is None:
+            print("No benchmark %s found." % (benchmarkName,))
+            session.rollback()
+            sys.exit(1)
 except MultipleResultsFound as e:
     print("Database inconsistency detected, multiple benchmarks/solvers found!")
     print(e)
     session.rollback()
     sys.exit(1)
-   
-# create Run object
-solverRun = dbobj.Run(benchmark=benchmark, solver_version=solver, command_line=" ".join(solverArguments), complete=False)
-# create a Result object for each case
-for benchmarkCase in benchmark.cases:
-    result = dbobj.RunResult(run = solverRun, case = benchmarkCase, complete=False, solver_status=None, solver_output=None, solver_stderr=None, completion_time=None, hostname=None)
 
-session.commit()
-print("Scheduled run of {} for {}-{} (ID {}).".format(benchmarkName, solverName, solverVersionName, solverRun.id))
+if runAllBenchmarks:
+    allBenchmarks = session.query(dbobj.Benchmark)
+    if allBenchmarks == []:
+        print("No benchmarks are available.")
+        session.rollback()
+        sys.exit(1)
+    runs = []
+    for benchmark in allBenchmarks:
+            # create Run object
+        solverRun = dbobj.Run(benchmark=benchmark, solver_version=solver, command_line=" ".join(solverArguments), complete=False)
+        runs.append(solverRun)
+        # create a Result object for each case
+        for benchmarkCase in benchmark.cases:
+            result = dbobj.RunResult(run = solverRun, case = benchmarkCase, complete=False, solver_status=None, solver_output=None, solver_stderr=None, completion_time=None, hostname=None)
+        session.commit()
+        print("Scheduled run of {} for {}-{} (ID {}).".format(benchmarkName, solverName, solverVersionName, solverRun.id))
+    for run in runs:
+        runmanagement.resume(session, solverRun)
+else:
+    # create Run object
+    solverRun = dbobj.Run(benchmark=benchmark, solver_version=solver, command_line=" ".join(solverArguments), complete=False)
+    # create a Result object for each case
+    for benchmarkCase in benchmark.cases:
+        result = dbobj.RunResult(run = solverRun, case = benchmarkCase, complete=False, solver_status=None, solver_output=None, solver_stderr=None, completion_time=None, hostname=None)
 
-# since we've created the entries for the run, we can use the resumption mechanism to start it for real
+    session.commit()
+    print("Scheduled run of {} for {}-{} (ID {}).".format(benchmarkName, solverName, solverVersionName, solverRun.id))
 
-runmanagement.resume(session, solverRun)
-print("Complete.")
-runmanagement.report(session, solverRun)
+    # since we've created the entries for the run, we can use the resumption mechanism to start it for real
+
+    runmanagement.resume(session, solverRun)
+    print("Complete.")
+    runmanagement.report(session, solverRun)
