7cae240134204924005a45a80b21e7f84afb31ce
Murphy Berzish <murphy.berzish@gmail.com>
2017-11-30 17:00:28 -0500

    add option to list all runs for a solver; print benchmark name in output

--- list-runs.py
+++ list-runs.py
@@ -12,6 +12,7 @@ def usage():
     print("With one argument, list all runs of the specified benchmark.")
     print("With a solver name, list all runs of the specified solver on the given benchmark.")
     print("With a solver name and version, list all runs of that version of the solver on the given benchmark.")
+    print("If benchmark is 'all', list all runs of the specified solver. Both solvername and version must be specified.")
 
 if len(sys.argv) == 2 and (sys.argv[1] == '--help' or sys.argv[1] == '-h'):
     usage()
@@ -38,7 +39,10 @@ elif len(sys.argv) == 4: # benchmark, solver name, version
     benchmarkName = sys.argv[1]
     solverName = sys.argv[2]
     solverVersion = sys.argv[3]
-    runs = session.query(dbobj.Run).join(dbobj.Benchmark).filter(dbobj.Benchmark.name == benchmarkName).join(dbobj.SolverVersion, dbobj.Solver).filter(dbobj.Solver.name == solverName).filter(dbobj.SolverVersion.version == solverVersion).order_by(dbobj.Run.startdate).all()
+    if benchmarkName == "all":
+        runs = session.query(dbobj.Run).join(dbobj.Benchmark).join(dbobj.SolverVersion, dbobj.Solver).filter(dbobj.Solver.name == solverName).filter(dbobj.SolverVersion.version == solverVersion).order_by(dbobj.Run.startdate).all()
+    else:
+        runs = session.query(dbobj.Run).join(dbobj.Benchmark).filter(dbobj.Benchmark.name == benchmarkName).join(dbobj.SolverVersion, dbobj.Solver).filter(dbobj.Solver.name == solverName).filter(dbobj.SolverVersion.version == solverVersion).order_by(dbobj.Run.startdate).all()
 else:
     usage()
     sys.exit(1)
@@ -59,7 +63,7 @@ for run in runs:
                 nSolvedCases += 1
                 totalTime += r.completion_time
         timeF = float(totalTime) / 1000.0
-        print("#{} {}-{} {} Incomplete ({}/{} done, {:.3f}s) {}".format(run.id, run.solver_version.solver.name, run.solver_version.version, run.startdate, nSolvedCases, nTotalCases, timeF, run.command_line))
+        print("#{} {} {}-{} {} Incomplete ({}/{} done, {:.3f}s) {}".format(run.id, run.benchmark.name, run.solver_version.solver.name, run.solver_version.version, run.startdate, nSolvedCases, nTotalCases, timeF, run.command_line))
     else:
         nSAT = 0
         nUNSAT = 0
@@ -93,5 +97,5 @@ for run in runs:
         timeF = float(totalTime) / 1000.0
         timeNoTimeoutF = float(totalTimeWithoutTimeouts) / 1000.0
         timeDisplay = "({:.3f}s | {:.3f}s+T/O)".format(timeNoTimeoutF, timeF)
-        print("#{} {}-{} {} {} {} {}".format(run.id, run.solver_version.solver.name, run.solver_version.version, run.startdate, timeDisplay, caseDisplay, run.command_line))
+        print("#{} {} {}-{} {} {} {} {}".format(run.id, run.benchmark.name, run.solver_version.solver.name, run.solver_version.version, run.startdate, timeDisplay, caseDisplay, run.command_line))
 
